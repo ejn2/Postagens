df18bda55d5f1f57c8937c101a28a808816a6739
ejn2 <eduardop9011@gmail.com>
2022-06-09 12:24:13 -0300

    foi implementado os métodos find

--- src/main/java/com/app/postagem/services/UserService.java
+++ src/main/java/com/app/postagem/services/UserService.java
@@ -1,5 +1,8 @@
 package com.app.postagem.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.app.postagem.dto.UserDTO;
 import com.app.postagem.exceptions.EmailIsAlreadyRegisteredException;
+import com.app.postagem.exceptions.UserNotFoundException;
 import com.app.postagem.models.UserModel;
 import com.app.postagem.repository.UserRepository;
 
@@ -27,6 +31,8 @@ public class UserService {
 
 	}
 	
+	// ========================== [ Save ] ==========================
+	
 	@Transactional
 	public UserDTO saveUser(UserDTO userDTO) throws EmailIsAlreadyRegisteredException {
 		this.verifyIfEmailIsAlreadyRegistered(userDTO.getEmail());
@@ -36,6 +42,28 @@ public class UserService {
 				);
 		
 		return this.modelMapper.map(user, UserDTO.class);
+	}
+	
+	
+	// ========================== [ Find All ] ==========================
+	
+	public List<UserDTO> listOfUsers() {
+		
+		return this.userRepository.findAll()
+			.stream()
+		 	.map(user -> this.modelMapper.map(user, UserDTO.class))
+		 	.collect(Collectors.toList());
+		
+	}
+	
+	// ========================== [ Find by id ] ==========================
+	
+	public UserDTO findById(Long id) throws UserNotFoundException {
+		UserModel user = this.userRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException("User not Found with id '"+ id+"'"));
+		
+		return this.modelMapper.map(user, UserDTO.class);
+		
 	}
 	
 }
--- src/test/java/com/app/postagem/services/UserServiceTest.java
+++ src/test/java/com/app/postagem/services/UserServiceTest.java
@@ -1,9 +1,12 @@
 package com.app.postagem.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.app.postagem.dto.UserDTO;
 import com.app.postagem.exceptions.EmailIsAlreadyRegisteredException;
+import com.app.postagem.exceptions.UserNotFoundException;
 import com.app.postagem.models.UserModel;
 import com.app.postagem.repository.UserRepository;
 import com.app.postagem.utils.UserUtils;
@@ -29,7 +33,6 @@ public class UserServiceTest {
 	
 	UserModel userModel = UserUtils.newUser();
 	UserDTO userDTO = UserUtils.newDTO();
-	
 	
 	// ========================== [Test | Save ] ==========================
 	
@@ -60,5 +63,56 @@ public class UserServiceTest {
 			);
 		
 	}
+	
+	
+	// ========================== [Test | find all ] ==========================
+	
+	@Test
+	void whenListOfUsersIsCalled_ThenAListOfUserIsReturned() {
+		
+		List<UserModel> modelList = Collections.singletonList(this.userModel);
+		
+		when(this.userRepository.findAll())
+			.thenReturn(modelList);
+		
+		List<UserDTO> dtoList = this.userService.listOfUsers();
+		
+		assertEquals(modelList.get(0).getName(),
+				dtoList.get(0).getName());
+		
+		assertEquals(modelList.get(0).getEmail(),
+					dtoList.get(0).getEmail()
+				);
+	}
+	
+	// ========================== [Test | find by id ] ==========================
+	
+	@Test
+	void whenFindByIdIsCalledWithValidId_ThenAUserIsReturned() throws UserNotFoundException {
+		
+		when(this.userRepository.findById(Mockito.any(Long.class)))
+			.thenReturn(Optional.of(this.userModel));
+		
+		UserDTO foundUser = this.userService.findById(this.userDTO.getId());
+		
+		assertEquals(this.userDTO.getName(), foundUser.getName());
+		assertEquals(this.userDTO.getEmail(), foundUser.getEmail());
+		
+	}
+	
+
+	@Test
+	void whenFindByIdIsCalledWithInvalidId_ThenAnExceptionIsThrown() throws UserNotFoundException {
+		when(this.userRepository.findById(Mockito.any(Long.class)))
+			.thenReturn(Optional.empty());
+		
+		assertThrows(UserNotFoundException.class, () ->
+			this.userService.findById(1L)
+				);
+		
+		
+	}
+	
+	
 	
 }
